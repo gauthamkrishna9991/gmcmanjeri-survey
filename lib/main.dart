17f95558fa17264e9cc73c626e56beb9e83bdfdc
gauthamkrishna9991@live.com <Goutham Krishna K V>
2019-06-09 21:04:39 +0530

    Added Android Specific files. TODO: Add the dart file to  inside the lib/ folder as a package.

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:gmcmanjeri_survey/views/mainform.dart';
+import 'package:gmcmanjeri_survey/views/questionnaire.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "GMC Manjeri Survey",
+      debugShowCheckedModeBanner: false,
+      color: Colors.white,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.black)
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          )
+        ),
+        iconTheme: IconThemeData(color: Colors.black)
+      ),
+      home: MainForm(),
+    );
+  }
+}
